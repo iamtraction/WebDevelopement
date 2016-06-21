e4465bf4de84117859ed09d73213244a3a08df96
Sankarsan Kampa <snkrsnkampa@users.noreply.github.com>
2016-06-21 13:49:02 +0530

    Add files via upload

---
+++ fadeTransition.js
@@ -0,0 +1,16 @@
+var speed = 'slow';
+
+$('html, body').hide();
+
+$(document).ready(function() {
+    $('html, body').fadeIn(speed, function() {
+        $('a[href], button[href]').click(function(event) {
+            var url = $(this).attr('href');
+            if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+            event.preventDefault();
+            $('html, body').fadeOut(speed, function() {
+                window.location = url;
+            });
+        });
+    });
+});
