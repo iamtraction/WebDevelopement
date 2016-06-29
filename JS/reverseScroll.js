9b0b27d640119e222d454787dd18ac466474fa48
Sankarsan Kampa <snkrsnkampa@users.noreply.github.com>
2016-06-29 14:30:02 +0530

    Update reverseScroll.js

--- JS/reverseScroll.js
+++ JS/reverseScroll.js
@@ -1,5 +1,7 @@
 //Reverse Scroll html elements
-//add '.reverse-scroll' class to the elements you want to reverse-scroll
+//Created by @snkrsnkampa
+//Link this '.js' file from your html file.
+//Add '.reverse-scroll' class to the elements you want to reverse-scroll
 $(document).ready(function(){ 
  $(window).scroll(function(){
    $('.reverse-scroll').css('transform', 'translate3d(0,' + $(this).scrollTop()*0.2 + 'px, 0)');  //increase the value from '0.2' to increase scroll speed
