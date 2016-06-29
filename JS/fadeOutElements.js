5e4a5fc75050c8d698bfd72399a8a7b002d53bf5
Sankarsan Kampa <snkrsnkampa@users.noreply.github.com>
2016-06-29 14:11:18 +0530

    Update fadeOutElements.js

--- JS/fadeOutElements.js
+++ JS/fadeOutElements.js
@@ -1,4 +1,4 @@
 //Fade Out Elements while scrolling
 $(window).scroll(function(){
-  $(".fade-out").css("opacity", 1 - $(window).scrollTop() / 800); //Decrese the value from '800' to fade-out fast and vice-versa
+  $(".fade-out").css("opacity", 1 - $(window).scrollTop() / 800); //Decrese the value from '800' to fade-out fast and vice-versa and 0 for no fade effect
 });
