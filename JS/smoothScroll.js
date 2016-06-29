4a8b6758a46d7182d2c25502fb43e1b12ffd35c0
Sankarsan Kampa <snkrsnkampa@users.noreply.github.com>
2016-06-29 14:31:29 +0530

    Update smoothScroll.js

--- JS/smoothScroll.js
+++ JS/smoothScroll.js
@@ -1,4 +1,6 @@
-//Just add this script for smooth scrolling between inpage links/anchors
+//Smooth scrolling between inpage links/anchors
+//Created by @snkrsnkampa
+//Just link this '.js' file from your html file for this to work.
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
