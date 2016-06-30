b54d074a516ab25ebcdaa15a19c14be3df4b8829
Sankarsan Kampa <snkrsnkampa@users.noreply.github.com>
2016-06-30 11:38:31 +0530

    Added support for multiple css

--- JS/asyncCSS.js
+++ JS/asyncCSS.js
@@ -1,4 +1,5 @@
 // Asynchronously load non-critical css
 //Created by @snkrsnkampa
 function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
-loadCSS("//example.com/style.css"); //replace the url with the url of your css
+loadCSS("//example.com/style.css"), //replace the url with the url of your css
+loadCSS("//example.com/style2.css"); //similarly you can add multiple css files
